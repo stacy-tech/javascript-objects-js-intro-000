c1b34a8fa9716af874c3abca44f9ee7c5796420b
Stacy Daniel <stacydn23@gmail.com>
2020-09-18 03:15:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
 return playlist
 }
-function removeFromPlaylist(the playlist object, artistName){
-  delete playlist[artistName]
+function removeFromPlaylist( playlist, artistName){
+  var playlist= playlist;
+  delete playlist.artistName
   return playlist
 }
